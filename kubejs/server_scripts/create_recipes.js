d31c1f019653a77b911fa28e617063248765bafd
FUFU <147926477+QiaOq0v0@users.noreply.github.com>
2025-01-31 15:10:10 +1100

    Update create_recipes.js

--- kubejs/server_scripts/create_recipes.js
+++ kubejs/server_scripts/create_recipes.js
@@ -1,4 +1,4 @@
-﻿ServerEvents.recipes(event => {
+ServerEvents.recipes(event => {
     // 配方 1：咖啡液混合搅拌
     event.custom({
         "type": "create:mixing",
@@ -41,4 +41,24 @@
             }
         ]
     });
+    // 配方 3：分液池 - 将 Farmer's Respite 咖啡转换为咖啡液
+    event.custom({
+        "type": "create:emptying", // 配方类型为分液池 (Emptying)
+        "ingredients": [
+            {
+                "item": "farmersrespite:coffee" // 输入：Farmer's Respite 的咖啡
+            }
+        ],
+        "results": [
+            {
+                "fluid": "create:coffee_liq", // 输出流体：咖啡液
+                "amount": 27000 // 输出流体数量：27000（Fabric 单位）
+            },
+            {
+                "item": "minecraft:glass_bottle" // 掉落物品：玻璃瓶
+            }
+        ]
+    });
+
 });
+
