843a369a91bd334e712509cef19f77c63b49a9f1
QiaOq0v0 <qiaoqovo@163.com>
2025-01-31 14:43:19 +1100

    Jan-25

---
+++ kubejs/server_scripts/create_recipes.js
@@ -0,0 +1,44 @@
+﻿ServerEvents.recipes(event => {
+    // 配方 1：咖啡液混合搅拌
+    event.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "fluid": "minecraft:water",
+                "amount": 27000
+            },
+            {
+                "item": "farmersrespite:coffee_beans"
+            },
+            {
+                "item": "farmersrespite:coffee_beans"
+            }
+        ],
+        "results": [
+            {
+                "fluid": "create:coffee_liq",
+                "amount": 27000
+            }
+        ],
+        "heatRequirement": "heated"
+    });
+
+    // 配方 2：咖啡液灌装成 Farmer's Respite 咖啡
+    event.custom({
+        "type": "create:filling",
+        "ingredients": [
+            {
+                "item": "minecraft:glass_bottle"
+            },
+            {
+                "fluid": "create:coffee_liq",
+                "amount": 27000
+            }
+        ],
+        "results": [
+            {
+                "item": "farmersrespite:coffee"
+            }
+        ]
+    });
+});
